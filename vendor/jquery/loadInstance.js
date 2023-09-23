613c7ca0a4bf19bde560abde9863e0a347465f98
Trairat Udorn <IT66134@Pongsawadi.ac.th>
2023-09-23 23:23:14 +0700

    final change

--- vendor/jquery/loadInstance.js
+++ vendor/jquery/loadInstance.js
@@ -1,35 +1,12 @@
-let appHeader = `
-    <header class="header-area header-sticky">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <nav class="main-nav">
-                        <a href="index.html" class="logo">
-                            <img src="assets/images/logo.png">
-                        </a>
-                        <ul class="nav">
-                            <li><a href="index.html">หน้าแรก</a></li>
-                            <li><a href="about.html">เกี่ยวกับเรา</a></li>
-                            <li><a href="deals.html">ร้านค้า</a></li>
-                        </ul>   
-                        <a class="menu-trigger">
-                            <span>Menu</span>
-                        </a>
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </header>
-`;
-
 let appFooter = `
         <footer>
             <div class="container">
                 <div class="row">
                     <div class="col-lg-12">
-                    <p>Copyright © 2036 <a href="#">WoOx Travel</a> Company. All rights reserved.
-                        <br>Design: <a href="https://templatemo.com" target="_blank" title="free CSS templates">TemplateMo</a>
-                        Distribution: <a href="https://themewagon.com target=" _blank">ThemeWagon</a>
+                    <p>Copyright © 2036 <a href="#"><b>WoOx Travel</b></a> Company. All rights reserved.
+                        <br>Design: <a href="https://templatemo.com" target="_blank" title="free CSS templates"><b>TemplateMo</b></a>
+                        Distribution: <a href="https://themewagon.com target=" _blank"><b>ThemeWagon</b></a>
+                        <br>Powered by: <b>TRAIRAT UDORN</b> | <b>KHRIDSANA PUTTAKOSAI</b> | <b>HUSSAN RAKRACHDECH</b> | <b>NATTHAWUT TONAREE</b>
                     </p>
                     </div>
                 </div>
@@ -37,6 +14,4 @@ let appFooter = `
         </footer>
 `;
 
-
-document.getElementById("app-header").innerHTML = appHeader;
 document.getElementById("app-footer").innerHTML = appFooter;
